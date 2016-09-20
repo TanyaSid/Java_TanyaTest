bc153ea240f2d7fdc56495a0b8ed2a0df013deec
TanyaSid <sharsu_antea@bk.ru>
2016-09-20 16:39:06 +0300

    HW

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,15 @@
+/**
+ * Created by trodzina on 9/20/2016.
+ */
+package ru.stqa.pft.sandbox;
+
+public class Distance
+  {
+
+    public static void distanceCalculation(String[] args)
+    {
+
+      //System.out.println("Расстояние между точками с координатами " + a " и " +b + " = " + dist + );
+    }
+
+}
