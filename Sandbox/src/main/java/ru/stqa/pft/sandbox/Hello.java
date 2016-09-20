61dc9c7d96407ceb498bd50c728b074b83ca6a33
TanyaSid <sharsu_antea@bk.ru>
2016-09-20 14:17:05 +0300

    hello fix

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("ru.stqa.pft.sandbox.Hello everybody");
+        System.out.println("Hello everybody");
     }
 
 }
